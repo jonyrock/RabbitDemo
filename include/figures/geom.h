ca7dd84919c2a86877767edebb17df2d925e2a3f
jonyrock <ilms@live.ru>
2014-02-18 19:46:53 +0400

    clear up

--- include/figures/geom.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include <glm/glm.hpp>
-#include <vector>
-
-void fillCube(std::vector<glm::vec3>& vertices);
-void fillPlane(std::vector<glm::vec3>& vertices);
-void fillSphere(std::vector<glm::vec3>& vertices);
